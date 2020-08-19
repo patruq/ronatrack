48f59c09d7ad10276e759312448d2021ce24019f
Michael Deeb <michael.f.deeb@gmail.com>
2020-08-19 17:53:19 -0400

    added initial data layout and migrations

--- ronatrack/survey/models.py
+++ ronatrack/survey/models.py
@@ -1,11 +1,59 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+from django.utils.translation import ugettext_lazy as _
+from typedmodels.models import TypedModel
+from mptt.models import MPTTModel, TreeForeignKey
+
+User = get_user_model()
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Survey(models.Model):
+    name = models.CharField(max_length=200)
+    version = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+
+    class Meta:
+        unique_together = ['name', 'version']
 
 
-class Choice(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(TypedModel):
+    text = models.TextField(_("Text"))
+    choices = JSONField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, verbose_name=_("Category"), related_name="questions"
+    )
+    survey = models.ManyToManyField(
+        Survey,
+        through='SurveyItem',
+        through_fields=('question', 'survey'),
+    )
+
+
+class NumberRangeQuestion(Question):
+    pass
+
+
+class MultipleChoiceQuestion(Question):
+    pass
+
+
+class BooleanQuestion(Question):
+    pass
+
+
+class SurveyItem(MPTTModel):
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+
+class SurveyItemRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    item = models.ForeignKey(SurveyItem, on_delete=models.PROTECT)
+    answer = JSONField()
